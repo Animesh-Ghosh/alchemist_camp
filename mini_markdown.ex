2e34736095ac88cc5f09d1a39eeb5880280d3190
Animesh-Ghosh <animesh.ghosh@tuta.io>
2023-04-10 23:56:28 +0530

    Complete following screencast till new-markdown-tags

--- mini_markdown.ex
+++ mini_markdown.ex
@@ -1,13 +1,29 @@
 defmodule MiniMarkdown do
   def to_html(text) do
     text
+    |> h1
+    |> h2
     |> p
+    |> big
     |> bold
     |> italics
+    |> small
+  end
+
+  def h1(text) do
+    Regex.replace(~r{(\r|\n|\r\n|^)\# +([^#][^\n\r]+)}, text, "<h1>\\2</h1>")
+  end
+
+  def h2(text) do
+    Regex.replace(~r{(\r|\n|\r\n|^)\#\# +([^#][^\n\r]+)}, text, "<h2>\\2</h2>")
   end
 
   def p(text) do
-    Regex.replace(~r{(\r|\n|\r\n|^)+([^\r\n]+)((\r|\n|\r\n)+$)?}, text, "<p>\\2</p>")
+    Regex.replace(~r{(\r|\n|\r\n|^)+([^#<][^\r\n]+)((\r|\n|\r\n)+$)?}, text, "<p>\\2</p>")
+  end
+
+  def big(text) do
+    Regex.replace(~r{\+\+(.*)\+\+}, text, "<big>\\1</big>")
   end
 
   def bold(text) do
@@ -18,13 +34,27 @@ defmodule MiniMarkdown do
     Regex.replace(~r{\*(.*)\*}, text, "<em>\\1</em>")
   end
 
+  def small(text) do
+    Regex.replace(~r{--(.*)--}, text, "<small>\\1</small>")
+  end
+
   def test_string do
     """
-    I *so* enjoyed eating that burrito and the hot sauce was **amazing**!
+    # Main title of the doc
 
-    What did you think of it?
+    ## A less important header
 
-    asdf
+    I *really* don't like you!
+
+    This is **important**, so read along.
+
+    Here is a list of
+
+    1) One
+    2) Two
+    3) Three
+
+    ++big things++--small things--
     """
   end
 end
